e3e8e8391b4b99cdbf458249399ddc839fe551f7
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-02-25 14:16:17 +0100

    Just delete all existing tables before rebuild

--- ddb/rebuild.ts
+++ ddb/rebuild.ts
@@ -5,23 +5,22 @@
 import { DynamoDB } from '@aws-sdk/client-dynamodb'
 
 const endpoint = 'http://localhost:8000'
-const ddb = new DynamoDB({ endpoint, region: 'localhost' })
+const ddb = new DynamoDB({ endpoint, region: 'localhost' });
 
-const CX_TABLE = 'connections'
-ddb.describeTable({ TableName: CX_TABLE }, err => {
-  void (typeof err === 'undefined' ? Promise.resolve() : ddb.deleteTable({ TableName: CX_TABLE }))
-    .then(() => ddb.createTable({
-      TableName: CX_TABLE,
-      KeySchema: [
-        { AttributeName: 'primaryKey', KeyType: 'HASH' },
-      ],
-      AttributeDefinitions: [
-        { AttributeName: 'primaryKey', AttributeType: 'S' },
-      ],
-      ProvisionedThroughput: {
-        ReadCapacityUnits: 5,
-        WriteCapacityUnits: 5,
-      },
-    }))
-    .then(data => console.log('Table re-created:', data.TableDescription?.TableName))
-})
+(async function main() {
+  const CX_TABLE = 'connections'
+  await ddb.listTables({}).then(({ TableNames = [] }) => Promise.all(TableNames.map(name => ddb.deleteTable({ TableName: name }))))
+  await ddb.createTable({
+    TableName: CX_TABLE,
+    KeySchema: [
+      { AttributeName: 'primaryKey', KeyType: 'HASH' },
+    ],
+    AttributeDefinitions: [
+      { AttributeName: 'primaryKey', AttributeType: 'S' },
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 5,
+      WriteCapacityUnits: 5,
+    },
+  })
+}())
