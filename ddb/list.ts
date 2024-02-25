b4736a6298944a5c6280bcef353b456a04b25321
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-02-25 15:48:21 +0100

    Expect proper Sessions in list.ts

--- ddb/list.ts
+++ ddb/list.ts
@@ -5,9 +5,11 @@
 import { DynamoDB } from '@aws-sdk/client-dynamodb'
 import { unmarshall } from '@aws-sdk/util-dynamodb'
 
+import { type Session } from './types'
+
 const endpoint = 'http://localhost:8000'
 const ddb = new DynamoDB({ endpoint, region: 'localhost' });
 
 (async function main() {
-  await ddb.scan({ TableName: 'sessions' }).then(({ Items }) => Items?.map(i => unmarshall(i))).then(console.log)
+  await ddb.scan({ TableName: 'sessions' }).then(({ Items }) => Items?.map(i => unmarshall(i) as Session[])).then(console.log)
 }())
