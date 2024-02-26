cf43377892b14d4462fb1c4edba189c6d763fac5
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-02-26 14:36:47 +0100

    Set up dummy front-end playground

---
+++ fe/src/easy-htmlelement.ts
@@ -0,0 +1,112 @@
+import { type RegExpGroups } from './types'
+
+const SVGNS = 'http://www.w3.org/2000/svg'
+
+export default class EasyHTMLElement {
+
+  public readonly elem!: HTMLElement | SVGElement
+
+  constructor(base: string | HTMLElement | SVGElement) {
+    this.elem = typeof base === 'string' ? document.createElement(base) : base
+  }
+
+  public static anchor({ href, text }: { href: string, text: string }): EasyHTMLElement {
+    return new EasyHTMLElement('a').attrs({ href }).content(text)
+  }
+
+  public cls(...classes: string[]): this {
+    this.elem.classList.add(...classes.filter(cls => !!cls))
+
+    return this
+  }
+
+  public on<K extends keyof HTMLElementEventMap>(event: K, listener: (this: HTMLElement, ev: HTMLElementEventMap[K]) => unknown): this {
+    this.elem.addEventListener(event, listener as EventListener)
+
+    return this
+  }
+
+  public attrs(attributes: Record<string, { toString: () => string }>): this {
+    Object.entries(attributes).forEach(([k, v]) => this.elem.setAttribute(k, String(v)))
+
+    return this
+  }
+
+  public styles(styles: Record<string, { toString: () => string }>): this {
+    Object.entries(styles).forEach(([k, v]) => this.elem.style.setProperty(k, String(v)))
+
+    return this
+  }
+
+  public at(area: string): this {
+    return this.attrs({ 'grid-area': area }).styles({ 'grid-area': area }) // For ease of use with CSS selectors
+  }
+
+  /**
+   * Parses contents and:
+   * - discard empty strings
+   * - automatically mark for hyphenation (for `en-gb`) with `\u00AD` (soft hyphen),
+   * - replace linefeeds (literal `\n`) with `<br />` elements
+   * - replace markdown-style hyperlinks with `<a href="...">...</a>` elements
+   * - replace `&nbsp;` with `\u00A0` (non-breaking space)
+   */
+  public content(...contents: ReadonlyArray<string | EasyHTMLElement>): this {
+    this.elem.append(...contents
+      .filter(content => content !== '')
+      .flatMap(content => (typeof content !== 'string' ? content.elem : content
+        .replace(/&nbsp;/g, '\u00A0')
+        .split(/(?<=\[[^\]]+\]\([^)]+\))|(?=\[[^\]]+\]\([^)]+\))/) // split around markdown-style hyperlinks
+        .map(fragment => fragment.match(/^\[(?<text>[^\]]+)\]\((?<href>[^)]+)\)$/)?.groups as RegExpGroups<'text' | 'href'> | undefined ?? fragment)
+        .flatMap(fragment => (typeof fragment === 'string'
+          ? fragment.split(/\n/g).flatMap(t => [new EasyHTMLElement('br').elem, t]).slice(1)
+          : EasyHTMLElement.anchor(fragment).elem))
+      )))
+
+    return this
+  }
+
+}
+
+export function element(tag: keyof HTMLElementTagNameMap | HTMLElement = 'div'): EasyHTMLElement {
+  return new EasyHTMLElement(tag)
+}
+
+export function elementSVG(tag: keyof SVGElementTagNameMap = 'svg'): EasyHTMLElement {
+  return new EasyHTMLElement(document.createElementNS(SVGNS, tag))
+}
+
+export function div(...contents: ReadonlyArray<string | EasyHTMLElement>): EasyHTMLElement {
+  return new EasyHTMLElement('div').content(...contents)
+}
+
+export function span(...contents: ReadonlyArray<string | EasyHTMLElement>): EasyHTMLElement {
+  return new EasyHTMLElement('span').content(...contents)
+}
+
+export function anchor({ href, text }: { href: string, text: string }): EasyHTMLElement {
+  return EasyHTMLElement.anchor({ href, text })
+}
+export function section(id: string): EasyHTMLElement {
+  return new EasyHTMLElement('section').attrs({ id })
+}
+
+export function article(cls: string): EasyHTMLElement {
+  return new EasyHTMLElement('article').cls(cls)
+}
+
+/**
+ * Creates a new `<span />` element with the supplied `content`,
+ * unless the content already is a *single EasyHTMLElement*, in which case
+ * it won't be wrapped into a (meaningless) span.
+ */
+function make(...content: ReadonlyArray<string | EasyHTMLElement>): EasyHTMLElement {
+  return (content.length === 1 && typeof content[0] !== 'string') ? content[0] : span(...content)
+}
+
+export function lighter(...content: ReadonlyArray<string | EasyHTMLElement>): EasyHTMLElement {
+  return make(...content).cls('lighter')
+}
+
+export function lightest(...content: ReadonlyArray<string | EasyHTMLElement>): EasyHTMLElement {
+  return make(...content).cls('lightest')
+}
--- fe/src/main.ts
+++ fe/src/main.ts
@@ -1,3 +1,32 @@
-const d = document.createElement('div')
-d.innerHTML = 'Hello, World!'
-document.body.appendChild(d)
+import { type MessageLogin, type MessageVote } from '../../be/types'
+
+import { element } from './easy-htmlelement'
+
+const area = element('pre')
+
+const socket = new WebSocket('ws://localhost:8080')
+socket.onmessage = e => {
+  const currentTime = new Date().toISOString().slice(11, -1)
+  area.elem.innerHTML += `${currentTime}: ${String(e.data)}\n`
+}
+
+const input = element('input').attrs({ type: 'number', placeholder: 'Your vote' }).styles({ width: '100%' })
+const name = element('input').attrs({ type: 'text', placeholder: 'Your name' }).styles({ width: '100%' })
+const session = element('input').attrs({ type: 'session', placeholder: 'The session ID' }).styles({ width: '100%' })
+element(document.body).content(
+  area,
+  input,
+  element('button').content('Send').cls('button').styles({ width: '100%' })
+    .on('click', () => {
+      socket.send(JSON.stringify({ action: 'vote', item: 'asdf', vote: [Number((input.elem as HTMLInputElement).value), 0] } as MessageVote));
+      (input.elem as HTMLInputElement).value = ''
+    }),
+  name,
+  session,
+  element('button').content('Login').cls('button').styles({ width: '100%' })
+    .on('click', () => {
+      const ss = (session.elem as HTMLInputElement).value
+      socket.send(JSON.stringify({ action: 'login', user: (name.elem as HTMLInputElement).value, ss: ss.length > 0 ? ss : undefined } as MessageLogin));
+      (session.elem as HTMLInputElement).value = ''
+    }),
+)
