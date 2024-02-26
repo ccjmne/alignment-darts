bc5fc357d9e5a9467dee79688bb3777f92c5fb28
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-02-26 14:34:45 +0100

    Clean-up back-end server and login handler

--- be/login.ts
+++ be/login.ts
@@ -1,19 +1,22 @@
-import { type Session } from '../ddb/types'
-
+import { callbackAPI } from './callbackAPI'
 import { get, put } from './db'
-import { type Event } from './srv'
+import { type Event, type Response } from './srv'
 
-export default async function handler({ body: message, requestContext: { connectionId } }: Event): Promise<Session> {
+export default async function handler({ body: message, requestContext: { connectionId } }: Event): Promise<Response> {
   if (message.action !== 'login') {
     return Promise.reject(new Error('Invalid message type'))
   }
 
-  return typeof message.ss !== 'undefined'
+  return (typeof message.ss !== 'undefined'
     ? get(message.ss).then(({ cx, ...session }) => ({ ...session, cx: { ...cx, [connectionId]: message.user } }))
     : put({
       pk: crypto.randomUUID(),
       cx: { [connectionId]: message.user },
       cfg: { items: [], dimensions: [[0, 0], [0, 0]] },
       votes: {},
-    })
+    }))
+    .then(session => Promise.allSettled(
+      Object.keys(session.cx).map(cx => callbackAPI.postToConnection({ ConnectionId: cx, Data: JSON.stringify(session) }).promise()),
+    ))
+    .then(() => ({ statusCode: 200 }))
 }
--- be/srv.ts
+++ be/srv.ts
@@ -2,65 +2,57 @@
 
 /* eslint-disable no-console */
 
-import { WebSocketServer, type WebSocket } from 'ws'
+import { WebSocketServer } from 'ws'
 
 import { type CxId } from '../ddb/types'
 
+import { off, on } from './callbackAPI'
 import loginHandler from './login'
 import { type Message } from './types'
 
+export type Response = { statusCode: number, body?: string }
 export type Event = {
   body: Message;
   requestContext: { connectionId: CxId };
 }
 
 const wss = new WebSocketServer({ port: 8080 })
-const cx = new Map<CxId, WebSocket>()
-
-export default {
-  postToConnection({ ConnectionId, Data }: { ConnectionId: CxId, Data: string }): { promise: () => Promise<void> } {
-    return {
-      promise(): Promise<void> {
-        const ws = cx.get(ConnectionId)
-        return ws ? Promise.resolve(ws.send(Data)) : Promise.reject(new Error('No active connection with that id'))
-      },
-    }
-  },
-}
-
 wss.on('connection', ws => {
   const connectionId = crypto.randomUUID()
 
-  cx.set(connectionId, ws)
+  on(connectionId, ws)
   console.debug(`${new Date().toISOString().slice(11, -1)} - opened`)
 
   ws.on('close', () => {
-    cx.delete(connectionId)
+    off(connectionId)
     console.debug(`${new Date().toISOString().slice(11, -1)} - closed`)
   })
 
-  ws.on('message', (data: Message) => {
-    try {
-      const event: Event = { body: data, requestContext: { connectionId } }
+  ws.on('message', (buf: Buffer) => {
+    const data = JSON.parse(String(buf)) as Message
+    const event: Event = { body: data, requestContext: { connectionId } };
+    (function dispatch() {
       switch (data.action) {
       case 'login':
         console.debug(`User ${data.user} connected`)
-        void loginHandler(event)
-        break
+
+        return loginHandler(event)
 
       case 'config':
         console.debug('Config received')
-        break
+
+        return Promise.resolve()
 
       case 'vote':
         console.debug('Vote received')
-        break
+
+        return Promise.resolve()
 
       default:
-        console.debug('Unknown message')
+        console.debug('Unknown message', data)
+
+        return Promise.reject(new Error('Unknown message'))
       }
-    } catch (e) {
-      console.error(e)
-    }
+    }()).catch(console.error)
   })
 })
