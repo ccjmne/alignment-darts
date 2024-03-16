3485095cffc034c9973ce3803507d1cf52732486
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-03-16 20:19:48 +0100

    Broadcast the user's connection ID w/ the session

--- be/notify.ts
+++ be/notify.ts
@@ -6,5 +6,5 @@ import { callbackAPI } from './callbackAPI'
 export default function notify(session: Session): Promise<PromiseSettledResult<void>[]> {
   return Promise.allSettled(Object.values(session.usrs)
     .filter(hasKey('cx'))
-    .map(({ cx: ConnectionId }) => callbackAPI.postToConnection({ ConnectionId, Data: JSON.stringify(session) }).promise()))
+    .map(({ cx }) => callbackAPI.postToConnection({ ConnectionId: cx, Data: JSON.stringify({ ...session, cx }) }).promise()))
 }
